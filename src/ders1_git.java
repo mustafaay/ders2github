548477600f0e6a9156e95ec48718d034a79d016c
Mustafa Ay <mustafaay33.44@gmail.com>
2022-08-13 21:59:08 +0300

    github ucuncu  deneme 13.08.2022

--- src/ders1_git.java
+++ src/ders1_git.java
@@ -4,6 +4,7 @@ public class ders1_git {
 
         System.out.println("git hup  1. deneme");
         System.out.println("heloo git 2. deneme");
+        System.out.println("heloo git 3. deneme");
 
 
 
