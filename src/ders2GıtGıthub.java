2dbe8f383d8d35569e3e364cf69540d923dcaf1d
Mustafa Ay <mustafaay33.44@gmail.com>
2022-08-16 14:46:00 +0300

    der2 16.08/2022 deneme 1

---
+++ src/ders2GıtGıthub.java
@@ -0,0 +1,16 @@
+public class ders2GıtGıthub {
+    /*
+    1-) git init --> local repo olusturmak icin yani  . git ile klasorumuzun icindeki dosyalari isikendirmek icin kulanilir
+
+    2-) git add. --> working space'den(yani yerel ) dosyalarimizi  staging area'ya( yani comitlemek icin beklenen yer ) gonderir
+
+    3-) git commit -m" masaj" --> staging area'dan commit stora dosyalarimizi gondermek icin kulanilir(commıt yanı versıon yani surum olusturmus oluruz)
+
+    4-)git push. --> uzak repoya( yani remote - githup) gondermek icin kulaniriz
+    yanliz git push komutunu direk kulanmak istersek 1 kez
+                      git remote add origin ve bunu yanina githaptaki dosyani adresini yapistirmamiz gerekir
+                      git push -u origin master
+                      bu iki komutu tek sefer kulandiktan sonra sadece git push yazmamiz yeterlidir
+
+     */
+}
